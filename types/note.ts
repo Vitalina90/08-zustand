d9f6f5ef484eae50405c42ed848d39f1bb43f4e3
Vitalina Sinkova <vsinkovaa@gmail.com>
2025-07-05 13:25:21 +0300

    Initial commit

---
+++ types/note.ts
@@ -0,0 +1,13 @@
+import type { Tag } from "@/lib/constants";
+
+export interface Note {
+  id: number;
+  title: string;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+  tag: Tag;
+}
+
+
+
